43bdf529111dd20829a9178d034fb577f4196d5e
carlyle-felix <carlyle.felix@gmail.com>
2026-01-12 18:05:08 +0200

    raspin: use cli args

--- raspin/src/raspin.c
+++ raspin/src/raspin.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "../include/gpiod-utils.h"
 #include "../include/raspin-manager.h"
@@ -6,11 +7,21 @@
 int main(int argc, char *argv[]) {
     Line_list rpi;
 
+    if (argc == 1) {
+        printf("idk what to do.\n");
+        return 1;
+    }
+
     rpi = populate_pins();
     if (!rpi) {
         return 1;
     }
-    print_pins(rpi, PRINT_LIST);
+
+    if (!strcmp(argv[1], "list")) {
+        print_pins(rpi, PRINT_LIST);
+    } else if (!strcmp(argv[1], "layout")) {
+        printf("slow down, tiger.\n");
+    }
     free_line_list(rpi);
     
     return 0;
