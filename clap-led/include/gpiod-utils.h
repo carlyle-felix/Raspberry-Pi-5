11177de0c75b9b5eb582822a508fcb08ded8305b
carlyle-felix <carlyle.felix@gmail.com>
2026-01-12 03:29:52 +0200

    clap-led: update gpiod-utils

--- clap-led/include/gpiod-utils.h
+++ clap-led/include/gpiod-utils.h
@@ -4,10 +4,16 @@
 #include <stdint.h>
 #include <gpiod.h>
 
+#define DEFAULT_CONSUMER "gpiod-utils"
+
+typedef struct gpio_pin *Gpio_pin;
+
+Gpio_pin new_line(const unsigned int gpio_line, const char *consumer);
+struct gpiod_line_request *pin_setup(struct gpiod_chip *chip, Gpio_pin *pin, enum gpiod_line_direction direction, enum gpiod_line_value initial_state);
+void release_line_items(Gpio_pin pin);
 struct gpiod_chip *chip_open(const char *str);
 struct gpiod_line_settings *line_state(enum gpiod_line_direction direction, enum gpiod_line_value initial_state);
-struct gpiod_line_config *configure_lines(const unsigned int *pins, const uint8_t num_pins, struct gpiod_line_settings *line_settings);
-struct gpiod_line_request *request_lines(struct gpiod_chip *chip, struct gpiod_line_config *line_config, \
-                                        struct gpiod_request_config **request_config, const char *consumer);
+struct gpiod_line_config *configure_lines(Gpio_pin *pin);
+struct gpiod_line_request *request_lines(struct gpiod_chip *chip, Gpio_pin *pin);
 
 #endif
--- clap-led/src/clap-led.c
+++ clap-led/src/clap-led.c
@@ -4,102 +4,67 @@
 #include "../include/gpiod-utils.h"
 
 #define CHIP "/dev/gpiochip0"
-#define CONSUMER "clap-led"
-
-#define PIN11 17
-#define PIN16 23
-const unsigned int led[] = {17};
-const unsigned int sens[] = {23};
-
+#define LED_CONSUMER "clap-led: LED"
+#define LED_PIN11 17
+#define SENSOR_CONSUMER "clap-led: sound sensor"
+#define SENSOR_PIN16 23
 
 int main(void)
 {
     struct gpiod_chip *chip;
-    struct gpiod_line_settings *led_line_settings, *sens_line_settings;
-    struct gpiod_line_config *led_line_config, *sens_line_config;
-    struct gpiod_request_config *led_request_config, *sens_request_config;
-    struct gpiod_line_request *led_request, *sens_request;
-    uint8_t n;
+    struct gpiod_line_request *sensor_request, *led_request;
+    Gpio_pin sensor, led;
 
+    // chip setup
     chip = chip_open(CHIP);
     if (!chip) {
         return 1;
     }
 
-    led_line_settings = line_state(GPIOD_LINE_DIRECTION_OUTPUT, GPIOD_LINE_VALUE_INACTIVE);
-    if (!led_line_settings) {
+    // sensor setup
+    sensor = new_line(SENSOR_PIN16, SENSOR_CONSUMER);
+    if (!sensor) {
         gpiod_chip_close(chip);
         return 1;
     }
-
-    led_line_config = configure_lines(led, 1, led_line_settings);
-    if (!led_line_config) {
+    sensor_request = pin_setup(chip, &sensor, GPIOD_LINE_DIRECTION_INPUT, GPIOD_LINE_VALUE_INACTIVE);
+    if (!sensor) {
+        release_line_items(sensor);
         gpiod_chip_close(chip);
-        gpiod_line_settings_free(led_line_settings);
+    }
+
+    // led setup
+    led = new_line(LED_PIN11, LED_CONSUMER);
+    if (!led) {
+        release_line_items(sensor);
+        gpiod_chip_close(chip);
         return 1;
     }
-
-    led_request = request_lines(chip, led_line_config, &led_request_config, CONSUMER);
-    if (!led_request) {
+    led_request = pin_setup(chip, &led, GPIOD_LINE_DIRECTION_OUTPUT, GPIOD_LINE_VALUE_INACTIVE);
+    if (!led) {
+        release_line_items(led);
+        release_line_items(sensor);
         gpiod_chip_close(chip);
-        gpiod_line_settings_free(led_line_settings);
-        gpiod_line_config_free(led_line_config);
     }
 
-    sens_line_settings = line_state(GPIOD_LINE_DIRECTION_INPUT, GPIOD_LINE_VALUE_INACTIVE);
-    if (!sens_line_settings) {
-        gpiod_chip_close(chip);
-        gpiod_line_settings_free(led_line_settings);
-        gpiod_line_config_free(led_line_config);
-        gpiod_request_config_free(led_request_config);
-        gpiod_line_request_release(led_request);
-        return 1;
+    // begin logic
+    while (!gpiod_line_request_get_value(sensor_request, SENSOR_PIN16)) {
+        usleep(25000);
+    }
+    printf("sound detected.\n");
+
+    if (gpiod_line_request_set_value(led_request, LED_PIN11, GPIOD_LINE_VALUE_ACTIVE)) {
+        perror("failed to change GPIO pin state.");
+    }
+    sleep(1);
+    if (gpiod_line_request_set_value(led_request, LED_PIN11, GPIOD_LINE_VALUE_INACTIVE)) {
+        perror("failed to change GPIO pin state.");
     }
 
-    sens_line_config = configure_lines(sens, 1, sens_line_settings);
-    if (!sens_line_config) {
-        gpiod_chip_close(chip);
-        gpiod_line_settings_free(led_line_settings);
-        gpiod_line_config_free(led_line_config);
-        gpiod_request_config_free(led_request_config);
-        gpiod_line_request_release(led_request);
-        gpiod_line_settings_free(sens_line_settings);
-        return 1;
-    }
-
-    sens_request = request_lines(chip, sens_line_config, &sens_request_config, CONSUMER);
-    if (!sens_request) {
-        gpiod_chip_close(chip);
-        gpiod_line_settings_free(led_line_settings);
-        gpiod_line_config_free(led_line_config);
-        gpiod_request_config_free(led_request_config);
-        gpiod_line_request_release(led_request);
-        gpiod_line_settings_free(sens_line_settings);
-        gpiod_line_config_free(sens_line_config);
-    }
-
-    for (n = 0; n < 10; n++) {
-        while (!gpiod_line_request_get_value(sens_request, PIN16)) {
-            usleep(25000);
-        }
-
-        printf("sound detected.\n");
-        if (gpiod_line_request_set_value(led_request, PIN11, GPIOD_LINE_VALUE_ACTIVE)) {
-            perror("failed to change GPIO pin state.");
-        }
-        sleep(1);
-        if (gpiod_line_request_set_value(led_request, PIN11, GPIOD_LINE_VALUE_INACTIVE)) {
-            perror("failed to change GPIO pin state.");
-        }
-    }
-    
+    // free
+    release_line_items(led);
+    release_line_items(sensor);
     gpiod_chip_close(chip);
-    gpiod_line_settings_free(led_line_settings);
-    gpiod_line_config_free(led_line_config);
-    gpiod_request_config_free(led_request_config);
-    gpiod_line_request_release(led_request);
-    gpiod_line_settings_free(sens_line_settings);
-    gpiod_line_config_free(sens_line_config);
-    gpiod_request_config_free(sens_request_config);
-    gpiod_line_request_release(sens_request);
+
+    return 1;
 }
